0bbe41355c74605d220c0038683082a580822ea6
Mac Book Air M1 <macbookairm1@Macs-MacBook-Air.local>
2025-10-15 18:16:18 +0700

    Add Choosen Us Part

---
+++ src/components/ChooseUs/index.jsx
@@ -0,0 +1,13 @@
+import { MainWrapper } from "./styles";
+
+
+export function ChooseUs() {
+
+    return (
+        <>
+            <MainWrapper>
+                
+            </MainWrapper>
+        </>
+    )
+}
